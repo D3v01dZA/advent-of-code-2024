14b35055b98a27678bd9c4c3266761cb0799cb1a
Christopher Altona <caltona1@gmail.com>
2024-12-03 09:28:54 -0500

    Day 2 part 2

--- c02/src/main.rs
+++ c02/src/main.rs
@@ -18,38 +18,50 @@ fn main() {
         let split: Vec<i32> = row.split_whitespace()
             .map(|x| x.parse().expect("Parse failed"))
             .collect();
-        if is_safe(split) {
+        if is_safe(&split) {
             safe = safe + 1;
+        } else {
+            // This is absolutely terrible but I don't have time to do backtracking - recursion is
+            // probably the way to go, just have to handle the first and second element then slurp
+            // the array
+            for i in 0..split.len() {
+                let mut split_clone = split.clone();
+                split_clone.remove(i);
+                if is_safe(&split_clone) {
+                    safe = safe + 1;
+                    break;
+                }
+            }
         }
     }
 
     println!("Safe {}", safe);
 }
 
-fn is_safe(reports: Vec<i32>) -> bool {
+fn is_safe(reports: &Vec<i32>) -> bool {
     let mut safe = true;
     let mut previous: Option<i32> = None;
     let mut increasing: Option<bool> = None;
     for report in reports {
         if previous.is_none() {
-            previous = Some(report);
-        } else if previous.unwrap() == report {
+            previous = Some(report.clone());
+        } else if previous.unwrap() == *report {
             safe = false;
             break;
-        } else if (previous.unwrap() - report).abs() > 3 {
+        } else if (previous.unwrap() - *report).abs() > 3 {
             safe = false;
             break;
         } else if increasing.is_none() {
-            increasing = Some(previous.unwrap() < report);
-            previous = Some(report);
-        } else if increasing.unwrap() && previous.unwrap() > report  {
+            increasing = Some(previous.unwrap() < *report);
+            previous = Some(report.clone());
+        } else if increasing.unwrap() && previous.unwrap() > *report  {
             safe = false;
             break;
-        } else if !increasing.unwrap() && previous.unwrap() < report  {
+        } else if !increasing.unwrap() && previous.unwrap() < *report  {
             safe = false;
             break;
         } else {
-            previous = Some(report);
+            previous = Some(report.clone());
         }
     }
     return safe;
