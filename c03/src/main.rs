2ba9c4f9eabd29a9a83f8fd7f96b594459594222
Christopher Altona <caltona1@gmail.com>
2024-12-03 17:39:36 -0500

    Day 3 part 1

---
+++ c03/src/main.rs
@@ -0,0 +1,67 @@
+use std::{fs::File, io::Read};
+
+fn read() -> Vec<String> {
+    let mut file = File::open("input.txt").expect("No file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Couldn't read file");
+    return contents.split('\n')
+        .map(|x| x.to_string())
+        .filter(|x| x.len() > 0)
+        .collect();
+}
+
+fn main() {
+    let rows = read();
+    
+    let mut total = 0;
+    let mut left: Vec<char> = vec![];
+    let mut right: Vec<char> = vec![];
+    let mut progress = 0;
+    for row in rows {
+        for c in row.chars() {
+            match progress {
+                0 => if c == 'm' { progress += 1 } else { progress = 0 },
+                1 => if c == 'u' { progress += 1 } else { progress = 0 },
+                2 => if c == 'l' { progress += 1 } else { progress = 0 },
+                3 => if c == '(' { progress += 1 } else { progress = 0 },
+                4 => if c.is_digit(10) { 
+                    progress += 1;
+                    left.push(c);
+                } else {
+                    progress = 0;
+                    left.clear();
+                },
+                5 => if c.is_digit(10) {
+                    left.push(c);
+                } else if c == ',' {
+                    progress += 1;
+                } else {
+                    progress = 0;
+                    left.clear();
+                },
+                6 => if c.is_digit(10) {
+                    right.push(c);
+                } else if c == ')' {
+                    progress = 0;
+                    let l: String = left.clone().into_iter().collect();
+                    let r: String = right.clone().into_iter().collect();
+                    left.clear();
+                    right.clear();
+                    let l: i32 = l.parse().expect("Parse failed");
+                    let r: i32 = r.parse().expect("Parse failed");
+                    total += l * r;
+                } else if c == 'm' {
+                    progress = 1;
+                    left.clear();
+                    right.clear();
+                } else {
+                    progress = 0;
+                    left.clear();
+                    right.clear();
+                }
+                _ => {},
+            };
+        }
+    }
+    println!("Total {}", total);
+}
