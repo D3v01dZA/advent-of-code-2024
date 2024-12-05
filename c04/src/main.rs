9d3f23aca91854e2d760fb3f2db9b619baffa804
Christopher Altona <caltona1@gmail.com>
2024-12-04 22:38:50 -0500

    Day 4 part 2

--- c04/src/main.rs
+++ c04/src/main.rs
@@ -20,37 +20,8 @@ fn main() {
 
     for i in 0..height {
         for j in 0..width {
-            if rows[i as usize][j as usize] == 'X' {
-                // left -> right
-                if is_mas(&rows, height, width, i, j, 0, 1) {
-                    total += 1;
-                }
-                // right -> left
-                if is_mas(&rows, height, width, i, j, 0, -1) {
-                    total += 1;
-                }
-                // top -> bottom
-                if is_mas(&rows, height, width, i, j, 1, 0) {
-                    total += 1;
-                }
-                // bottom -> top
-                if is_mas(&rows, height, width, i, j, -1, 0) {
-                    total += 1;
-                }
-                // top left -> bottom right
-                if is_mas(&rows, height, width, i, j, 1, 1) {
-                    total += 1;
-                }
-                // top right -> bottom left
-                if is_mas(&rows, height, width, i, j, 1, -1) {
-                    total += 1;
-                }
-                // bottom left -> top right
-                if is_mas(&rows, height, width, i, j, -1, 1) {
-                    total += 1;
-                }
-                // bottom right -> top left
-                if is_mas(&rows, height, width, i, j, -1, -1) {
+            if rows[i as usize][j as usize] == 'A' {
+                if is_mas(&rows, height, width, i, j) {
                     total += 1;
                 }
             }
@@ -60,24 +31,72 @@ fn main() {
     println!("Total {total}");
 }
 
-fn is_mas(
+fn is_mas(rows: &[Vec<char>], height: i32, width: i32, height_loc: i32, width_loc: i32) -> bool {
+    let mut left: Vec<char> = vec![];
+    let mut right: Vec<char> = vec![];
+    let mutated = mutation(rows, left, height, width, height_loc, width_loc, 1, 1);
+    if mutated.is_none() {
+        return false;
+    } else {
+        left = mutated.unwrap();
+    }
+    left.push('A');
+    let mutated = mutation(rows, left, height, width, height_loc, width_loc, -1, -1);
+    if mutated.is_none() {
+        return false;
+    } else {
+        left = mutated.unwrap();
+    }
+    let mutated = mutation(rows, right, height, width, height_loc, width_loc, -1, 1);
+    if mutated.is_none() {
+        return false;
+    } else {
+        right = mutated.unwrap();
+    }
+    right.push('A');
+    let mutated = mutation(rows, right, height, width, height_loc, width_loc, 1, -1);
+    if mutated.is_none() {
+        return false;
+    } else {
+        right = mutated.unwrap();
+    }
+    let str: String = left.iter().collect();
+    if str != "MAS" {
+        left.reverse();
+        let str: String = left.iter().collect();
+        if str != "MAS" {
+            return false;
+        }
+    }
+    let str: String = right.iter().collect();
+    if str != "MAS" {
+        right.reverse();
+        let str: String = right.iter().collect();
+        if str != "MAS" {
+            return false;
+        }
+    }
+    true
+}
+
+// This sucks so hard the previous solution was so clean but the borrow checker got mad at me then
+// me at it, you could do a for (vec, height_mod, width_mod) in [[vec, 1, 1], vec[-1, -1]] etc if
+// it would just let you
+fn mutation(
     rows: &[Vec<char>],
+    mut vec: Vec<char>,
     height: i32,
     width: i32,
     height_loc: i32,
     width_loc: i32,
     height_mod: i32,
     width_mod: i32,
-) -> bool {
-    let mut vec: Vec<char> = vec![];
-    for i in 1..4 {
-        let h: i32 = height_loc + (height_mod * i);
-        let w: i32 = width_loc + (width_mod * i);
-        if h >= height || w >= width || h < 0 || w < 0 {
-            return false;
-        }
-        vec.push(rows[h as usize][w as usize]);
+) -> Option<Vec<char>> {
+    let h: i32 = height_loc + height_mod;
+    let w: i32 = width_loc + width_mod;
+    if h >= height || w >= width || h < 0 || w < 0 {
+        return None;
     }
-    let string: String = vec.iter().collect();
-    string == "MAS"
+    vec.push(rows[h as usize][w as usize]);
+    Some(vec)
 }
