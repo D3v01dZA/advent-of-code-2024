d305e70f49dbfe1c783716f7acfd6c5c5204efd8
Christopher Altona <caltona1@gmail.com>
2024-12-06 17:41:44 -0500

    Day 5 part 1

---
+++ c05/src/main.rs
@@ -0,0 +1,57 @@
+use std::{collections::HashMap, fs::File, io::Read};
+
+fn read() -> Vec<String> {
+    let mut file = File::open("input.txt").expect("No file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Couldn't read file");
+    return contents
+        .split('\n')
+        .map(|x| x.to_string())
+        .filter(|x| !x.is_empty())
+        .collect();
+}
+
+fn main() {
+    let rows = read();
+
+    let mut precendences: Vec<(i32, i32)> = vec![];
+    let mut updates: Vec<Vec<i32>> = vec![];
+
+    for row in rows {
+        if row.contains('|') {
+            let split: Vec<i32> = row
+                .split('|')
+                .map(|x| x.parse().expect("Parse failed"))
+                .collect();
+            precendences.push((split[0], split[1]));
+        } else {
+            let split: Vec<i32> = row
+                .split(',')
+                .map(|x| x.parse().expect("Parse failed"))
+                .collect();
+            updates.push(split);
+        }
+    }
+
+    let mut total = 0;
+    for update in updates {
+        let mut indices: HashMap<i32, usize> = HashMap::new();
+        for (index, page) in update.iter().enumerate() {
+            indices.insert(*page, index);
+        }
+        let mut valid = true;
+        for (before, after) in &precendences {
+            if let (Some(before_index), Some(after_index)) =
+                (indices.get(before), indices.get(after))
+            {
+                valid &= before_index < after_index;
+            }
+        }
+        if valid {
+            total += update[update.len() / 2];
+        }
+    }
+
+    println!("Total {total}")
+}
